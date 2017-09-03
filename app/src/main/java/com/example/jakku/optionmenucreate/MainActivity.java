0be0411aeb812f03c20c014c1f914b955d3f2226
Rajesh Jakkula <Rajesh Jakkula>
2017-09-02 20:02:18 -0500

    My First Commit

---
+++ app/src/main/java/com/example/jakku/optionmenucreate/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.jakku.optionmenucreate;
+
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+      ImageView imageView;
+        int[] imageResIds = {R.drawable.saipallavi, R.drawable.saipallavi2, R.drawable.saipallavi3};
+        int imageIndex = 0;
+        boolean color = true;
+        boolean red = true;
+        boolean green = true;
+        boolean blue = true;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            imageView = (ImageView)findViewById(R.id.imageview);
+            loadImage();
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        Drawable drawable = menu.findItem(R.id.nextImage).getIcon();
+        menu.findItem(R.id.red).setChecked(red);
+        menu.findItem(R.id.green).setChecked(green);
+        menu.findItem(R.id.blue).setChecked(blue);
+        menu.setGroupVisible(R.id.colorGroup, color);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nextImage:
+                imageIndex++;
+                if (imageIndex >= imageResIds.length)
+                    imageIndex = 0;
+                loadImage();
+                break;
+            case R.id.color:
+                color=!color;
+                updateSaturation();
+                invalidateOptionsMenu();
+                break;
+            case R.id.red:
+                red = !red;
+               updateColors();
+                item.setChecked(red);
+                break;
+            case R.id.green:
+                red = !green;
+                updateColors();
+                item.setChecked(green);
+                break;
+            case R.id.blue:
+                red = !blue;
+                updateColors();
+                item.setChecked(blue);
+                break;
+            case R.id.reset:
+                imageView.clearColorFilter();
+                red=green=blue=color=true;
+                invalidateOptionsMenu();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+/*  @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem menuItem = menu.add("Next Image" );
+        //menuItem = menu.add("Another Image");
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menuItem.setIcon(R.drawable.ic_add_a_photo_black_24dp);
+       // menuItem.setIcon(R.drawable.ic_arrow_drop_down_circle_black_24dp);
+        menuItem.getIcon().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+        return true;
+    }*/
+
+    private void loadImage() {
+            Glide.with(this).load(imageResIds[imageIndex]).into(imageView);
+        }
+
+        private void updateSaturation() {
+            ColorMatrix colorMatrix = new ColorMatrix();
+            if (color) {
+                red = green = blue = true;
+                colorMatrix.setSaturation(1);
+            } else {
+                colorMatrix.setSaturation(0);
+            }
+            ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+            imageView.setColorFilter(colorFilter);
+        }
+
+        private void updateColors() {
+            ColorMatrix colorMatrix = new ColorMatrix();
+            float[] matrix = {
+                    1, 0, 0, 0, 0,
+                    0, 1, 0, 0, 0,
+                    0, 0, 1, 0, 0,
+                    0, 0, 0, 1, 0,
+            };
+            if (!red) matrix[0] = 0;
+            if (!green) matrix[6] = 0;
+            if (!blue) matrix[12] = 0;
+            colorMatrix.set(matrix);
+            ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+            imageView.setColorFilter(colorFilter);
+        }
+    }
+
+
